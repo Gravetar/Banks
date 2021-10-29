079424d83e49914e5c52b3f640a5db9f50d27769
Mihail <darkserj18@gmail.com>
2021-10-29 16:08:02 +0300

    add operator

--- Banks/AtmMachine.cs
+++ Banks/AtmMachine.cs
@@ -9,8 +9,29 @@ namespace Banks
     class AtmMachine
     {
         /// <summary>
+        /// Состояния банкомата
+        /// </summary>
+        public StateAtm stateAtm = StateAtm.on;
+
+        public double Cash = 0;
+
+        /// <summary>
         /// Транзакции банкомата
         /// </summary>
         public List<Transaction> Transactions = new List<Transaction>();
+
+        public void replenishCash(double value)
+        {
+            Cash += value;
+        }
+
     }
+
+    enum StateAtm
+    {
+        on, off
+    }
+
+
 }
+
---
+++ Banks/Operator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banks
+{
+    class Operator
+    {
+
+        public void AddCash(ref AtmMachine Atm, double value)
+        {
+            Atm.replenishCash(value);
+        }
+        public void StartAtm(ref AtmMachine Atm)
+        {
+            Atm.stateAtm = StateAtm.on;
+        }
+        public void StopAtm(ref AtmMachine Atm)
+        {
+            Atm.stateAtm = StateAtm.off;
+        }
+
+    }
+}
